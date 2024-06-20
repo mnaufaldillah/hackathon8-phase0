ec55a8113c7347748b324e1c80610f4f49b36211
mnaufaldillah <mnaufaldillah@gmail.com>
2024-06-20 21:03:38 +0700

    Music Control

---
+++ index.js
@@ -0,0 +1,18 @@
+const playMusic = document.getElementById('music-play');
+const controlMusic = document.getElementById('music-control');
+console.log(controlMusic);
+let toggle = true;
+
+playMusic.addEventListener('click', function(){
+    toggle = !toggle
+
+    if (toggle) {
+        playMusic.src = 'assets/music_background_play.png';
+        controlMusic.muted = false;
+        controlMusic.autoplay = true;
+
+    } else {
+        playMusic.src = 'assets/music_background_mute.png';
+        controlMusic.muted = true;
+    }
+})
